71e2c410eb2395e3b6e1e9ca86bd001a697acb0c
Danil <matweew.ol.danya@gmail.com>
2022-04-17 18:05:57 +0300

    add routing package

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import { IndexPage } from '../indexPage'
 import styles from './.module.css'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className={styles.App} >
-      
-    </div>
+    <BrowserRouter>
+      <div className={styles.App} >
+        <Routes>
+          <Route path='/' element={<IndexPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/indexPage/index.tsx
@@ -0,0 +1,16 @@
+import styles from './.module.css'
+// const IndexPage: React.ReactNode = (
+//     <div>
+//         <h1 className={styles.header} >Moonow</h1>
+//     </div>
+// )
+
+function IndexPage () {
+    return (
+        <div>
+            <h1 className={styles.header} >Moonow</h1>
+        </div>
+    );
+}
+
+export { IndexPage }
