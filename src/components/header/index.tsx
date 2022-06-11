4492cb9ffbb170dc1d7ee5f252296b1204bb7853
Danil <matweew.ol.danya@gmail.com>
2022-06-11 17:18:18 +0300

    add header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './.module.css'
+
+export const Header = () => {
+    return (
+        <header>
+            <h1 className={styles.header} >Moonow</h1>
+        </header>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './components/app'
+import { App } from './views/app'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
